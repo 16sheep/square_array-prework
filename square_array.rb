2eac6242dbb1f8383812b04f5ec9189bd2865480
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-08 06:47:40 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -4,3 +4,10 @@ def square_array(array)
   array.each {|num| new_arr << num * num}
   new_arr
 end
+
+def square_array(array)
+  # your code here
+  new_arr = []
+  array.each {|num| new_arr << num * num}
+  new_arr
+end
